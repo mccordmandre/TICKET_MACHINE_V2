542beb3fe35c109e9780051b59cb4d523e488905
Filipa <47zh7vjxym@privaterelay.appleid.com>
2026-05-19 20:45:04 +0100

    Delete KOTLIN/Stations.kt

--- KOTLIN/Stations.kt
+++
@@ -1,3 +0,0 @@
-val stations = listOf("Lisboa", "Madrid" , "Paris", "London", "Roma", "Berlin",
-    "Moskva", "Bruxelles", "Amsterdam", "Kyiv", "Athina", "Wien", "Warszawa",
-    "Kobenhavn","Stockholm", "Constantinople")
